d1e6749e578cb9e4ed76152a056fd20d6f1d1b9d
dilshod <dilshodx516@gmail.com>
2024-11-03 17:30:44 +0500

    food

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,9 +1,17 @@
 import "./Loading.scss"
 
-const Loading = () => {
+const Loading = ({ count = 3 }) => {
     return (
-        <div>
-            <div className="lds-ripple"><div></div><div></div></div>
+        <div className="flex flex-wrap justify-center gap-[30px]">
+            {
+                Array(count).fill("").map((_, inx) => (
+                    <div key={inx} className="flex flex-col gap-5 w-[300px] h-[450px] rounded-2xl border border-slate-500 p-3" >
+                        <div className="w-full h-48 bg-slate-300 rounded-lg"></div>
+                        <div className="w-[90%] h-10 bg-slate-300 rounded-lg"></div>
+                        <div className="w-full h-28 bg-slate-300 rounded-lg mt-6"></div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./Menu.scss"
 import { toast } from "react-toastify"
 import Loading from "../loading/Loading"
 import { useFetch } from "../hooks/useFetch"
 
 const Menu = () => {
+    const count = 3
     const [category, setCategory] = useState("/")
     const [offset, setOffset] = useState(1)
-    const { data, loading, error } = useFetch(`/products${category}`, { limit: offset * 3 }, [offset, category])
+    const { data, loading, error } = useFetch(`/products${category}`, { limit: offset * count }, [offset, category])
     const { data: categories } = useFetch("products/category-list")
-    console.log(category); 
+
+    useEffect(() => {
+        setOffset(1)
+    }, [category])
 
 
     const productsItems = data?.products?.map((pro) => (
@@ -29,7 +33,7 @@ const Menu = () => {
     const categoryItems = categories?.map((cate) => (
         <li
             onClick={() => setCategory(`/category/${cate}`)}
-            className="category_list"
+            className={`category_list ${`/category/${cate}` === category ? "bg-gray-300" : "text-black"}`}
             key={cate}>
             {cate}
         </li >
@@ -40,21 +44,30 @@ const Menu = () => {
             <h2 className="menu_title">Food Full of treaty Love</h2>
             <p className="menu_desc">There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, </p>
             <ul className="category">
+                <li
+                    onClick={() => setCategory("/")}
+                    className="all">
+                    All
+                </li>
                 {categoryItems}
             </ul>
             <div className="menu_wrapper">
                 {productsItems}
             </div>
-            {loading && <Loading />}
-            <button
-                disabled={loading}
-                onClick={() => setOffset(p => p + 1)}
-                className="more disabled:bg-red-500 disabled:cursor-not-allowed">
-                {
-                    loading ? "Loading..." : "Learn More"
-                }
-            </button>
-        </div>
+            {loading && <Loading count={count} />}
+
+            {
+                offset * count < data?.total &&
+                <button
+                    disabled={loading}
+                    onClick={() => setOffset(p => p + 1)}
+                    className="more disabled:bg-red-500 disabled:cursor-not-allowed">
+                    {
+                        loading ? "Loading..." : "Learn More"
+                    }
+                </button>
+            }
+        </div >
     )
 }
 
